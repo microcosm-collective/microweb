d8bd17be20d9bb1a7b90a16f6acb9c77dfcd16cc
Pascal Opitz <contact@pascalopitz.com>
2021-06-08 06:22:43 +0930

    Actually setting the value of the input when adding or modifying the file list
    I'm so so very sorry

--- core/static/js/attachments.js
+++ core/static/js/attachments.js
@@ -64,7 +64,7 @@
         return;
       }
 
-      this.input.files = ArraytoFileList(this.stack);
+      this.input.files = ArraytoFileList(files);
       this.callback_counter = files.length;
 
       // ugly way of keeping track of the reader.onload async events
