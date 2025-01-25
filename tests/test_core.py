94281a3438507e36602a87675f8ddeffcbec317c
Tom Ward <tomward@fmail.co.uk>
2025-01-25 17:36:42 +0000

    Run existing tests & a new very simple test in CI

---
+++ tests/test_core.py
@@ -0,0 +1,14 @@
+from core.api.resources import join_path_fragments
+from pytest import raises
+
+def test_api_resources_join_path_fragments():
+    joined = join_path_fragments(['path','to','nowhere'])
+    assert joined == "/path/to/nowhere"
+
+    joined = join_path_fragments(['double','/','slash'])
+    assert joined == "/double//slash"
+
+    with raises(Exception):
+        joined = join_path_fragments(['bad','mix/ed','slash'])
+
+
