76b3fda5833534a27972e347559686d3f378f670
Tom Ward <tomward@fmail.co.uk>
2025-01-28 14:42:05 +0000

    Add an endpoint test with minimal mock JSON

--- tests/test_core.py
+++ tests/test_core.py
@@ -2,6 +2,7 @@ from pytest import raises
 from httmock import urlmatch, HTTMock
 import re
 import pdb
+import json
 
 from core.api.resources import join_path_fragments, Site, Attachment
 
@@ -19,13 +20,46 @@ def test_api_resources_join_path_fragments():
 
 @urlmatch(netloc=r'(.*\.)?microco\.sm$')
 def microcosm_mock(url, request):
-    return "myarse"
+    if url.path == '/api/v1/hosts/microco.sm':
+        return "yepyep"
+    if url.path == '/api/v1/comments/1/attachments':
+        return json.dumps(
+            {
+                "error": "",
+                "data": {
+                    "attachments": {
+                        "total": 0,
+                        "limit": 100,
+                        "offset": 0,
+                        "maxOffset": 0,
+                        "totalPages": 1,
+                        "page": 1,
+                        "type": "",
+                    },
+                    "meta": {
+                        "created": "",
+                        
+                    },
+                    "items": []
+                }
+            }
+        )
 
 def test_api_resources_resolve_cname():
-    # nb. the host "microc.osm" will resolve to e.g. 
+    # nb. the host "microc.osm" will resolve to e.g.
     # "http://microco.sm/api/v1/hosts/microc.osm"
-    test_host = "microc.osm"
+    test_host = "microco.sm"
     with HTTMock(microcosm_mock):
         blah = Site.resolve_cname(test_host)
-        assert blah == "myarse"
+        assert blah == "yepyep"
 
+
+def test_attachment_retrieve():
+    # nb. set local_settings API_SCHEME to http
+    # & the value of test_host to match settings.API_DOMAIN_NAME
+    test_host = "microco.sm"
+    c_id = 1
+    with HTTMock(microcosm_mock):
+        c_attachments = Attachment.retrieve(test_host, "comments", c_id)
+
+    assert c_attachments.items == []
